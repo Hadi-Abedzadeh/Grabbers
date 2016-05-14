0f8d0fe5a0dccddb6e5f115523b0f339b7fd8b29
David Refoua <David@Refoua.me>
2016-05-14 20:52:22 +0430

    Updated get-proxy.php method
    Changelog:
    1) The proxies are outputted to the page while retrieving
    2) Now can also be opened with PHP CLI from console
    3) Fixed a bug where files could be located in a wrong directory
    4) The correct content-type is set while loading in browser
    5) The amount of pages can be set from the script, instead of multiple lines
    6) Major improvements regarding page fetches, progress can now be seen
    7) No temporarily files are required to store the HTML content, they are processed on the fly
    
    TL;DR: This version incorporates several improvements, and both major and minor bugs fixed.

--- get-proxy.php
+++ get-proxy.php
@@ -1,36 +1,27 @@
 <?php
-/*								 					   			    
-*  _____                      _____           _     _               
-* |  __ \                    / ____|         | |   | |              
-* | |__) | __ _____  ___   _| |  __ _ __ __ _| |__ | |__   ___ _ __ 
-* |  ___/ '__/ _ \ \/ / | | | | |_ | '__/ _` | '_ \| '_ \ / _ \ '__|
-* | |   | | | (_) >  <| |_| | |__| | | | (_| | |_) | |_) |  __/ |   
-* |_|   |_|  \___/_/\_\\__, |\_____|_|  \__,_|_.__/|_.__/ \___|_|   
-*                       __/ | Hadi Abedzadeh                        
-*                      |___/                                        
-*                                     						 	 	
-* Open this file on web browser                                     
-*/
+/**							 					   			         
+ *  _____                      _____           _     _               
+ * |  __ \                    / ____|         | |   | |              
+ * | |__) | __ _____  ___   _| |  __ _ __ __ _| |__ | |__   ___ _ __ 
+ * |  ___/ '__/ _ \ \/ / | | | | |_ | '__/ _` | '_ \| '_ \ / _ \ '__|
+ * | |   | | | (_) >  <| |_| | |__| | | | (_| | |_) | |_) |  __/ |   
+ * |_|   |_|  \___/_/\_\\__, |\_____|_|  \__,_|_.__/|_.__/ \___|_|   
+ *                       __/ | Hadi Abedzadeh                        
+ *                      |___/                                        
+ *                                     						 	 	 
+ * Open this file on web browser or console                          
+ */
 set_time_limit(0);
 error_reporting(0);
+chdir(dirname(__FILE__));
+header("Content-Type: text/plain");
 require_once('PHP_Simple_HTML_DOM.php');
-$page01 = file_get_html("https://proxy-list.org/english/index.php?p=1");
-$page02 = file_get_html("https://proxy-list.org/english/index.php?p=2");
-$page03 = file_get_html("https://proxy-list.org/english/index.php?p=3");
-$page04 = file_get_html("https://proxy-list.org/english/index.php?p=4");
-$page05 = file_get_html("https://proxy-list.org/english/index.php?p=5");
-$page06 = file_get_html("https://proxy-list.org/english/index.php?p=6");
-$page07 = file_get_html("https://proxy-list.org/english/index.php?p=7");
-$page08 = file_get_html("https://proxy-list.org/english/index.php?p=8");
-$page09 = file_get_html("https://proxy-list.org/english/index.php?p=9");
-$a = array($page01, $page02, $page03, $page04, $page05, $page06, $page07, $page08, $page09);
-file_put_contents("getproxy.txt",$a);
-$r = file_get_html('getproxy.txt');
-foreach ($r->find('li[class=proxy] <script type="text/javascript">') as $html) {
-    $a = (explode("'", $html));
-    $a = base64_decode($a[1]);
-    $a = $a . "\n";
-    file_put_contents('checked.txt', $a, FILE_APPEND);
-    unlink("getproxy.txt");
-    }
+$path = "https://proxy-list.org/english/index.php";
+$splitter = 'li[class=proxy] <script type="text/javascript">';
+for ($i = 1; $i <10; $i++)
+foreach (file_get_html($path."?p=$i")->find($splitter) as $html) {
+	$info = explode("'", $html);
+	$info = base64_decode($info[1]) . "\n"; echo $info; flush();
+	file_put_contents('checked.txt', $info, FILE_APPEND | LOCK_EX);
+	}
 ?>
